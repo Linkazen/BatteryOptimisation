5d8e1ed0a17c36b42a1a13d3e3ea6ec74496541f
Linkazen <jpch2k4@gmail.com>
2023-02-10 23:50:08 +0000

    Finished the sending emails script.

---
+++ MyME3000.py
@@ -0,0 +1,27 @@
+# Highbanks ME3000
+
+SLAVE=0x01
+THRESHOLD_FILE="/home/pi/ME3000/pct.txt"
+SERIAL_PORT="/dev/ttyUSB0"
+MIN_CHARGE=20
+MAX_CHARGE=99
+
+# MQTT
+ME3000_NAME='me3000'
+MQTT_HOST='localhost'
+MQTT_USER='emonpi'
+MQTT_PWD='emonpimqtt2016'
+
+TOPICS = ((6, 'voltage', 'H'),
+          (7, 'current', 'h'),
+          (14, 'batt_voltage', 'H'),
+          (15, 'batt_current', 'h'),
+          (16, 'batt_capacity', 'H'),
+          (17, 'batt_temp', 'H'),
+          (25, 'sell_grid', 'H'),
+          (26, 'buy_grid', 'H'),
+          (27, 'load_use', 'H'),
+          (36, 'charge_total', 'H'),
+          (37, 'discharge_total', 'H')
+         )
+  
--- SendInfo.py
+++ SendInfo.py
@@ -1,13 +1,13 @@
 import smtplib, ssl
 import json
+import datetime
 from dateutil import parser
-from datetime import datetime
 from SensitiveData import *
+
+message = f"Subject: Tariff Prices\n\nDate: {(datetime.datetime.now() + datetime.timedelta(days=1)).strftime('%m/%d')}\n\n"
 
 smtp_server = "smtp.gmail.com"
 port = 465
-
-message = ""
 
 context = ssl.create_default_context()
 
@@ -22,14 +22,20 @@ def formatEmail():
 
         Prices = TariffData["results"]
 
-        Prices = list(filter(lambda Obj : parser.parse(Obj["valid_from"], ignoretz=True) >= datetime.now().replace(hour=23, minute=0, second=0, microsecond=0), Prices))
-        print(Prices)
+        Prices = list(filter(lambda Obj : parser.parse(Obj["valid_from"], ignoretz=True) >= datetime.datetime.now().replace(hour=23, minute=0, second=0, microsecond=0), Prices))
 
-# def SortPrices(PricesArr):
-    
+        Prices.reverse()
+
+        global message
+        for price in Prices:
+            message += f'Price (inc vat): {price["value_inc_vat"]}\r\nPeriod: {parser.parse(price["valid_from"]).strftime("%H:%M")}-{parser.parse(price["valid_to"]).strftime("%H:%M")}\r\n\r\n'
+        
+
+formatEmail()
+
+print(message)
 
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(email_sender, password)
     server.sendmail(email_sender, email_receiver, message)
 
-formatEmail()
